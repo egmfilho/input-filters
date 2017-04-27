4bc35661385d5bd5977e3c262f37332085e11ef0
egmfilho <edumig12@hotmail.com>
2017-04-27 16:59:00 -0300

    bug fix & improvments

--- src/filters.js
+++ src/filters.js
@@ -2,65 +2,68 @@
  * Created by egmfilho on 24/08/16.
  */
 
-(function() {
+ (function() {
 
-'use strict';
+ 	'use strict';
 
-angular.module('egmfilho.inputFilters', [ ])
-  .directive('numberOnly', ['$locale', function($locale) {
-    return {
-			restrict: 'A',
-      require: 'ngModel',
-      link: function(scope, element, attr, ngModelCtrl) {
-        ngModelCtrl.$parsers.push(function(text) {
-          var separator = attr.separator ? attr.separator : $locale.NUMBER_FORMATS.DECIMAL_SEP,
-              transformedInput = text.replace(new RegExp('[^0-9' + separator + ']', 'g'), '');
-          if (transformedInput !== text) {
-            ngModelCtrl.$setViewValue(transformedInput);
-            ngModelCtrl.$render();
-          }
-          return transformedInput;
-        });
-      }
-    };
-  }])
-	.directive('currency', ['$filter', '$locale', function($filter, $locale) {
+ 	angular.module('egmfilho.inputFilters', [ ])
+ 	.directive('numberOnly', ['$locale', function($locale) {
+ 		return {
+ 			restrict: 'A',
+ 			require: 'ngModel',
+ 			link: function(scope, element, attr, ngModelCtrl) {
+ 				ngModelCtrl.$parsers.push(function(text) {
+ 					var separator = attr.separator ? attr.separator : $locale.NUMBER_FORMATS.DECIMAL_SEP,
+ 					transformedInput = text.replace(new RegExp('[^0-9' + separator + ']', 'g'), '');
+ 					if (transformedInput !== text) {
+ 						ngModelCtrl.$setViewValue(transformedInput);
+ 						ngModelCtrl.$render();
+ 					}
+ 					return transformedInput;
+ 				});
+ 			}
+ 		};
+ 	}])
+ 	.directive('currency', ['$filter', '$locale', function($filter, $locale) {
 
-    return {
-			restrict: 'A',
-      require: 'ngModel',
-      link: function(scope, element, attrs, ngModelController) {
+ 		return {
+ 			restrict: 'A',
+ 			require: 'ngModel',
+ 			link: function(scope, element, attrs, ngModelController) {
 
-        function parseToModel(data) {
-          if (data) {
-            return parseFloat(data.toString().replace(scope.$eval(attrs.symbol) || $locale.NUMBER_FORMATS.CURRENCY_SYM, '')
-              .replace($locale.NUMBER_FORMATS.GROUP_SEP, '')
-              .replace($locale.NUMBER_FORMATS.DECIMAL_SEP, '.'));
-          }
-          return parseFloat(data);
-        }
+ 				var symbol = scope.$eval(attrs.symbol) || '';
 
-        function parseToView(data) {
-          return $filter('currency')(parseToModel(data.replace(scope.$eval(attrs.symbol), '')), scope.$eval(attrs.symbol));
-        }
+ 				function parseToModel(data) {
 
-        ngModelController.$parsers.push(parseToModel);
+ 					if (!isNaN(parseFloat(data)) && data.toString().indexOf('.') != -1) {
+ 						return parseFloat(data);
+ 					} else {
+ 						return parseFloat(data.toString().replace(symbol || $locale.NUMBER_FORMATS.CURRENCY_SYM, '')
+ 							.replace($locale.NUMBER_FORMATS.GROUP_SEP, '')
+ 							.replace($locale.NUMBER_FORMATS.DECIMAL_SEP, '.'));
+ 					}
 
-        element.bind('blur', function() {
-          if (ngModelController.$viewValue) {
-            ngModelController.$viewValue = parseToView(ngModelController.$viewValue);
-            ngModelController.$render();
-          }
-        });
+ 				}
 
-        ngModelController.$formatters.push(function(data) {
-          if (data != null) {
-            return parseToView(data.toString());
-          }
-          return data;
-        });
-      }
-    };
-  }]);
+ 				ngModelController.$parsers.push(parseToModel);
 
-}());
+ 				function parseToView(data) {
+ 					if (data != null) {
+ 						return $filter('currency')(parseToModel(data.toString().replace(symbol, '')), symbol);
+ 					}
+ 					return data;
+ 				}
+
+ 				ngModelController.$formatters.push(parseToView);
+
+ 				element.bind('blur', function() {
+ 					if (ngModelController.$viewValue) {
+ 						ngModelController.$viewValue = parseToView(ngModelController.$viewValue);
+ 						ngModelController.$render();
+ 					}
+ 				});
+ 			}
+ 		};
+ 	}]);
+
+ }());
